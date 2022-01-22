c1c12d65371a4955028c9cbdf5345277bafb175c
m4belle <mabelletang.2020@scis.smu.edu.sg>
2022-01-22 18:06:49 +0800

    updated new and improved

---
+++ script.js
@@ -0,0 +1,9 @@
+function alertUser(msg) {
+    alert(msg);
+    }
+
+n =  new Date();
+y = n.getFullYear();
+m = n.getMonth() + 1;
+d = n.getDate();
+document.getElementById("date").innerHTML = m + "/" + d + "/" + y;
